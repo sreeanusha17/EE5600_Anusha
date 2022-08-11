e2313235f6aea5228d2bec789e153e88b923657a
sreeanusha17 <57354216+sreeanusha17@users.noreply.github.com>
2022-08-11 19:05:45 +0530

    Add files via upload

---
+++ Assignment 2/codes/gxrand.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+
+//Gaussian random numbers
+gaussian("gau.dat", 1000000);
+
+//Mean of uniform distribution
+printf("Mean Value: %lf \n",mean("gau.dat"));
+//Variance of uniform distribution
+printf("Variance Value: %lf \n",variance("gau.dat"));
+
+return 0;
+}
+
+
