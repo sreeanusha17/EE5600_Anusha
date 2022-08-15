e58b5da3d82e9102581db73a74fda7fbd32bcd5e
sreeanusha17 <57354216+sreeanusha17@users.noreply.github.com>
2022-08-15 18:25:21 +0530

    Add files via upload

---
+++ Assignment 4/codes/pdf_plot.py
@@ -0,0 +1,41 @@
+#Importing numpy, scipy, mpmath and pyplot
+import numpy as np
+import mpmath as mp
+import scipy 
+import matplotlib.pyplot as plt
+
+maxrange=60
+maxlim=6.0
+x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
+simlen = int(1e6) #number of samples
+err = [] #declaring probability list
+pdf = [] #declaring pdf list
+h = 2*maxlim/(maxrange-1);
+
+
+# numpy.random.uniform() method
+r_1 = np.random.uniform(size=10**6)
+r_2= np.random.uniform(size=10**6)
+randvar = r_1+r_2
+
+for i in range(0,maxrange):
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
+
+	
+for i in range(0,maxrange-1):
+	test = (err[i+1]-err[i])/(x[i+1]-x[i])
+	pdf.append(test) #storing the pdf values in a list
+
+plt.plot(x[0:(maxrange-1)],pdf)
+plt.grid() #creating the grid
+plt.xlabel('$x$')
+plt.ylabel('$p_X(x)$')
+plt.title("Probability Density Function")
+
+#plt.show()
+
+plt.savefig('../figs/t_pdf.pdf')
+plt.savefig('../figs/t_pdf.eps')
+
---
+++ Assignment 4/codes/txrand.py
@@ -0,0 +1,36 @@
+#Importing numpy, scipy, mpmath and pyplot
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd 
+import numpy as np
+
+# numpy.random.uniform() method
+r_1 = np.random.uniform(size=10**6)
+r_2= np.random.uniform(size=10**6)
+
+
+x = np.linspace(-4,4,30)#points on the x axis
+simlen = int(1e6) #number of samples
+err = [] #declaring probability list
+#randvar = np.random.normal(0,1,simlen)
+randvar = r_1+r_2
+#randvar = np.loadtxt('gau.dat',dtype='double')
+
+#########################################      CDF    ################################################
+for i in range(0,30):
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
+
+	
+plt.plot(x.T,err)#plotting the CDF
+plt.grid() #creating the grid
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
+plt.title("Cumulative Distribution Function")
+#plt.show()
+#if using termux
+plt.savefig('../figs/t_cdf.pdf')
+plt.savefig('../figs/t_cdf.eps')
+
